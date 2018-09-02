48905d54ae2e90f1b2736648a508503902106c10
jianlis <1184608655@qq.com>
2018-09-02 11:23:22 +0800

    提交

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-require('styles/App.css');
-
-import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/Main';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-// Render the main component into the dom
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
