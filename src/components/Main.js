c080fb83a32d6c07a5f054ef80b60b4b1b5b1816
jianlis <1184608655@qq.com>
2018-09-02 10:35:37 +0800

    提交

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,20 @@
-require('normalize.css/normalize.css');
-require('styles/App.css');
-
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
+class SomeThing extends React.Component {
+  render() {
+    return (
+      <div>
+        我是子组件
+      </div>
+    );
+  }
+}
 
 class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+        <SomeThing></SomeThing>
       </div>
     );
   }
