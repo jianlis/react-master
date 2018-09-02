ea3c504d2619d2d235aacf0265fc6e1d2020c48f
jianlis <1184608655@qq.com>
2018-09-02 12:21:03 +0800

    提交

--- src/module/App/App.js
+++ src/module/App/App.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from '../../assets/logo.svg';
 import './App.css';
+
+class SomeTgun extends Component {
+  render() {
+    return (
+      <div>
+        hello world
+      </div>
+    )
+  }
+}
 
 class App extends Component {
   render() {
@@ -13,6 +23,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <SomeTgun></SomeTgun>
       </div>
     );
   }
