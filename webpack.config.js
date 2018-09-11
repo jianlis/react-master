35650e63e1add7e46ca5dcad2f78d43b5da5ec1e
jianlis <1184608655@qq.com>
2018-09-11 16:09:12 +0800

    1

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,9 @@
-'use strict';
+var path = require('path')
 
-const path = require('path');
-const args = require('minimist')(process.argv.slice(2));
-
-// List of allowed environments
-const allowedEnvs = ['dev', 'dist', 'test'];
-
-// Set the correct environment
-let env;
-if (args._.length > 0 && args._.indexOf('start') !== -1) {
-  env = 'test';
-} else if (args.env) {
-  env = args.env;
-} else {
-  env = 'dev';
+module.exports = {
+    entry: ['./src/index'], // 在 index 檔案後的 .js 副檔名是可選的
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    }
 }
-process.env.REACT_WEBPACK_ENV = env;
-
-/**
- * Build the webpack configuration
- * @param  {String} wantedEnv The wanted environment
- * @return {Object} Webpack config
- */
-function buildConfig(wantedEnv) {
-  let isValid = wantedEnv && wantedEnv.length > 0 && allowedEnvs.indexOf(wantedEnv) !== -1;
-  let validEnv = isValid ? wantedEnv : 'dev';
-  let config = require(path.join(__dirname, 'config/' + validEnv));
-  return config;
-}
-
-module.exports = buildConfig(env);
